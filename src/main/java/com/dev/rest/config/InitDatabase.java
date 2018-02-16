c7a592c59df8a36c2b455f67ee45c7e988438c76
Димо Дрънгов <dimodrangov@gmail.com>
2018-02-16 08:49:15 +0200

    Update InitDatabase.java

--- src/main/java/com/dev/rest/config/InitDatabase.java
+++ src/main/java/com/dev/rest/config/InitDatabase.java
@@ -1,6 +1,7 @@
 package com.dev.rest.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -11,16 +12,22 @@ import com.dev.rest.services.UserService;
 @Component
 public class InitDatabase implements CommandLineRunner {
 
+	@Value("${init.admin.username}")
+	private String username;
+	
+	@Value("${init.admin.password}")
+	private String password;
+	
 	@Autowired
 	private UserService userService;
 	
 	@Override
 	public void run(String... arg0) throws Exception {
 		UserDTO dto = new UserDTO();
-		dto.setName("admin");
+		dto.setName(username);
 		dto.setAge(23);
 		dto.setEmail("testMail@host.bg");
-		dto.setPassword("admin");
+		dto.setPassword(password);
 		
 		userService.createUser(dto, UserRole.ROLE_ADMIN);
 	}
